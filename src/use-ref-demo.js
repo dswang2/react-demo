09363357b9fd72e5b1335b5eee767d0a77b9c2e2
wangdisheng <wangdishengyx@163.com>
2022-05-16 00:22:15 +0800

    react的第80课，useRef

--- src/use-ref-demo.js
+++ src/use-ref-demo.js
@@ -2,17 +2,24 @@ import React, { useEffect, useRef, useState } from "react";
 
 export default (props) => {
     const count = useRef(0); // count值的变化不会触发重新渲染
-    useEffect(() => {
-        count.current += 1; // 渲染完之后执行
-    });
-    console.log("count", count);
+    // useEffect(() => {
+    //     count.current += 1; // 渲染完之后执行
+    // });
+    const [_,set_] = React.useState(null)
     const [n, setN] = useState(0);
     const onclick = () => {
         setN(n => n + 1);
     }
+    const onclick2 = () => {
+        count.current += 1; // count的值不会自动刷新
+        set_(Math.random());
+    }
     return (
         <div className="H1">
-            <button onClick={onclick}>++</button>
-            <div>数据n={n}</div></div>
+            <button onClick={onclick}>n++</button>
+            <div>数据n:{n}</div>
+            <button onClick={onclick2}>count++</button>
+            <div>数据count:{count.current}</div>
+        </div>
     );
 }
